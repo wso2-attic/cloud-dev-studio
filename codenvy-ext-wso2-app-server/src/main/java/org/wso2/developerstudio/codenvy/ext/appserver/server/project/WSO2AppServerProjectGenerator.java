1c97a3d1a2711899479f43905fa19dcb1e7963ed
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2014-10-20 11:08:44 +0530

    Improved webapp generation.

--- codenvy-ext-wso2-app-server/src/main/java/org/wso2/developerstudio/codenvy/ext/appserver/server/project/WSO2AppServerProjectGenerator.java
+++ codenvy-ext-wso2-app-server/src/main/java/org/wso2/developerstudio/codenvy/ext/appserver/server/project/WSO2AppServerProjectGenerator.java
@@ -35,48 +35,65 @@ import java.util.Map;
 @Singleton
 public class WSO2AppServerProjectGenerator implements ProjectGenerator {
 
-    private static final Logger LOG = LoggerFactory.getLogger(WSO2AppServerProjectGenerator.class);
+	private static final Logger LOG = LoggerFactory.getLogger(WSO2AppServerProjectGenerator.class);
 
-    @Inject
-    public WSO2AppServerProjectGenerator(ProjectGeneratorRegistry registry) {
-        registry.register(this);
-    }
+	@Inject
+	public WSO2AppServerProjectGenerator(ProjectGeneratorRegistry registry) {
+		registry.register(this);
+	}
 
-    @Override
-    public String getId() {
-        return AppServerExtConstants.WSO2_APP_SERVER_PROJECT_GENERATOR_ID;
-    }
+	@Override
+	public String getId() {
+		return AppServerExtConstants.WSO2_APP_SERVER_PROJECT_GENERATOR_ID;
+	}
 
-    @Override
-    public void generateProject(FolderEntry folderEntry, Map<String, String> options) throws ForbiddenException,
-            ConflictException, ServerException {
+	@Override
+	public void generateProject(FolderEntry folderEntry, Map<String, String> options)
+			throws ForbiddenException,
+			       ConflictException, ServerException {
 
-        // TODO: Change below sample code to create sample projects as necessary
-        try {
+		// TODO: Change below sample code to create sample projects as necessary
+		try {
 
-            if (options.get(AppServerExtConstants.PROJECT_TYPE_ID).equals(AppServerExtConstants
-                    .WSO2_WEB_APP_PROJECT_ID)) {
+			if (options.get(AppServerExtConstants.PROJECT_TYPE_ID).equals(AppServerExtConstants
+					                                                              .WSO2_WEB_APP_PROJECT_ID)) {
 
-                folderEntry.createFile("webapp.txt", "App Server Web App Artifact.".getBytes(), "text/plain");
+				folderEntry.createFolder("src/main/webapp/");
+				folderEntry.createFolder("src/main/webapp/WEB-INF");
+				String content = "<web-app xmlns=\"http://java.sun.com/xml/ns/javaee\"\n" +
+				                 "\txmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+				                 "\txsi:schemaLocation=\"http://java.sun.com/xml/ns/javaee\n" +
+				                 "\thttp://java.sun.com/xml/ns/javaee/web-app_2_5.xsd\"\n" +
+				                 "\tversion=\"2.5\">\n" +
+				                 "</web-app>";
 
-            } else if (options.get(AppServerExtConstants.PROJECT_TYPE_ID).equals(AppServerExtConstants
-                    .WSO2_JAX_WS_PROJECT_ID)) {
-                folderEntry.createFile("jaxws.txt", "App Server JAXWS Service Artifact.".getBytes(), "text/plain");
+				folderEntry.createFile("src/main/webapp/WEB-INF/web.xml", content.getBytes(),
+				                       "text/xml");
 
-            } else if (options.get(AppServerExtConstants.PROJECT_TYPE_ID).equals(AppServerExtConstants
-                    .WSO2_JAX_RS_PROJECT_ID)) {
-                folderEntry.createFile("jaxrs.txt", "App Server JAXRS Service Artifact.".getBytes(), "text/plain");
-            } else {
-                LOG.error("Error generating project " + folderEntry.getName() + ".\nError : Project type is not " +
-                        "supported by " + getId() + ".");
-            }
+			} else if (options.get(AppServerExtConstants.PROJECT_TYPE_ID)
+			                  .equals(AppServerExtConstants
+					                          .WSO2_JAX_WS_PROJECT_ID)) {
+				folderEntry.createFile("jaxws.txt", "App Server JAXWS Service Artifact.".getBytes(),
+				                       "text/plain");
 
-        } catch (Exception e) {
-            LOG.error("Error generating project " + folderEntry.getName() + "\nError Msg : " + e.getLocalizedMessage
-                    (), e);
+			} else if (options.get(AppServerExtConstants.PROJECT_TYPE_ID)
+			                  .equals(AppServerExtConstants
+					                          .WSO2_JAX_RS_PROJECT_ID)) {
+				folderEntry.createFile("jaxrs.txt", "App Server JAXRS Service Artifact.".getBytes(),
+				                       "text/plain");
+			} else {
+				LOG.error("Error generating project " + folderEntry.getName() +
+				          ".\nError : Project type is not " +
+				          "supported by " + getId() + ".");
+			}
 
-        }
+		} catch (Exception e) {
+			LOG.error("Error generating project " + folderEntry.getName() + "\nError Msg : " +
+			          e.getLocalizedMessage
+					          (), e);
 
-    }
+		}
+
+	}
 
 }
