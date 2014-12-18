57cd39e0804e820f6bce84dc85e29ef681be2d92
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2014-12-18 17:36:48 +0530

    Refactoring cef client for linux64.

--- native/devs_cefclients/linux64/source/DevSCefBrowserEventHandler.h
+++ native/devs_cefclients/linux64/source/DevSCefBrowserEventHandler.h
@@ -1,6 +1,18 @@
-// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
-// reserved. Use of this source code is governed by a BSD-style license that
-// can be found in the LICENSE file.
+/*
+* Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 #ifndef CEF_TESTS_CEFSIMPLE_SIMPLE_HANDLER_H_
 #define CEF_TESTS_CEFSIMPLE_SIMPLE_HANDLER_H_
@@ -9,63 +21,63 @@
 
 #include <list>
 
-class DevSCefBrowserEventHandler : public CefClient,
-public CefDisplayHandler,
-public CefLifeSpanHandler,
-public CefLoadHandler {
-public:
-    DevSCefBrowserEventHandler();
-    ~DevSCefBrowserEventHandler();
+class DevSCefBrowserEventHandler :  public CefClient,
+                                    public CefDisplayHandler,
+                                    public CefLifeSpanHandler,
+                                    public CefLoadHandler {
+    public:
+        DevSCefBrowserEventHandler();
+        ~DevSCefBrowserEventHandler();
 
-    // Provide access to the single global instance of this object.
-    static DevSCefBrowserEventHandler* GetInstance();
+        // Provide access to the single global instance of this object.
+        static DevSCefBrowserEventHandler* GetInstance();
 
-    // CefClient methods:
+        // CefClient methods:
 
-    virtual CefRefPtr<CefDisplayHandler> GetDisplayHandler() OVERRIDE {
-        return this;
-    }
+        virtual CefRefPtr<CefDisplayHandler> GetDisplayHandler() OVERRIDE {
+            return this;
+        }
 
-    virtual CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() OVERRIDE {
-        return this;
-    }
+        virtual CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() OVERRIDE {
+            return this;
+        }
 
-    virtual CefRefPtr<CefLoadHandler> GetLoadHandler() OVERRIDE {
-        return this;
-    }
+        virtual CefRefPtr<CefLoadHandler> GetLoadHandler() OVERRIDE {
+            return this;
+        }
 
-    // CefDisplayHandler methods:
-    virtual void OnTitleChange(CefRefPtr<CefBrowser> browser,
-            const CefString& title) OVERRIDE;
+        // CefDisplayHandler methods:
+        virtual void OnTitleChange(CefRefPtr<CefBrowser> browser,
+                const CefString& title) OVERRIDE;
 
-    // CefLifeSpanHandler methods:
-    virtual void OnAfterCreated(CefRefPtr<CefBrowser> browser) OVERRIDE;
-    virtual bool DoClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
-    virtual void OnBeforeClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
+        // CefLifeSpanHandler methods:
+        virtual void OnAfterCreated(CefRefPtr<CefBrowser> browser) OVERRIDE;
+        virtual bool DoClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
+        virtual void OnBeforeClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
 
-    // CefLoadHandler methods:
-    virtual void OnLoadError(CefRefPtr<CefBrowser> browser,
-            CefRefPtr<CefFrame> frame,
-            ErrorCode errorCode,
-            const CefString& errorText,
-            const CefString& failedUrl) OVERRIDE;
+        // CefLoadHandler methods:
+        virtual void OnLoadError(CefRefPtr<CefBrowser> browser,
+                CefRefPtr<CefFrame> frame,
+                ErrorCode errorCode,
+                const CefString& errorText,
+                const CefString& failedUrl) OVERRIDE;
 
-    // Request that all existing browser windows close.
-    void CloseAllBrowsers(bool force_close);
+        // Request that all existing browser windows close.
+        void CloseAllBrowsers(bool force_close);
 
-    bool IsClosing() const {
-        return is_closing_;
-    }
+        bool IsClosing() const {
+            return is_closing_;
+        }
 
-private:
-    // List of existing browser windows. Only accessed on the CEF UI thread.
-    typedef std::list<CefRefPtr<CefBrowser> > BrowserList;
-    BrowserList browser_list_;
+    private:
+        // List of existing browser windows. Only accessed on the CEF UI thread.
+        typedef std::list<CefRefPtr<CefBrowser> > BrowserList;
+        BrowserList browser_list_;
 
-    bool is_closing_;
+        bool is_closing_;
 
-    // Include the default reference counting implementation.
-    IMPLEMENT_REFCOUNTING(DevSCefBrowserEventHandler);
+        // Include the default reference counting implementation.
+        IMPLEMENT_REFCOUNTING(DevSCefBrowserEventHandler);
 };
 
 #endif  // CEF_TESTS_CEFSIMPLE_SIMPLE_HANDLER_H_
--- native/devs_cefclients/linux64/source/DevSCefClient_app.h
+++ native/devs_cefclients/linux64/source/DevSCefClient_app.h
@@ -1,30 +1,42 @@
-// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
-// reserved. Use of this source code is governed by a BSD-style license that
-// can be found in the LICENSE file.
+/*
+* Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 #ifndef CEF_TESTS_CEFSIMPLE_SIMPLE_APP_H_
 #define CEF_TESTS_CEFSIMPLE_SIMPLE_APP_H_
 
 #include "include/cef_app.h"
 
-class DevSCefClient : public CefApp,
-public CefBrowserProcessHandler {
-public:
-    DevSCefClient();
+class DevSCefClient :   public CefApp,
+                        public CefBrowserProcessHandler {
+    public:
+        DevSCefClient();
 
-    // CefApp methods:
+        // CefApp methods:
 
-    virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler()
-    OVERRIDE {
-        return this;
-    }
+        virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler()
+        OVERRIDE {
+            return this;
+        }
 
-    // CefBrowserProcessHandler methods:
-    virtual void OnContextInitialized() OVERRIDE;
+        // CefBrowserProcessHandler methods:
+        virtual void OnContextInitialized() OVERRIDE;
 
-private:
-    // Include the default reference counting implementation.
-    IMPLEMENT_REFCOUNTING(DevSCefClient);
+    private:
+        // Include the default reference counting implementation.
+        IMPLEMENT_REFCOUNTING(DevSCefClient);
 };
 
 #endif  // CEF_TESTS_CEFSIMPLE_SIMPLE_APP_H_
