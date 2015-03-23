4226201e658eb42804ac0692e0df0b7832118a5a
tikaa <wnthksnrth@gmail.com>
2015-03-23 08:40:38 +0530

    fixing widnows build

---
+++ assembly/org.wso2.developerstudio.ide.assembly/resources/template/che-template-clone.bat
@@ -0,0 +1,33 @@
+#!/bin/bash
+# import multiple remote git repositories to local CODE dir
+
+# settings / change this to your config
+
+
+# if no output from the remote ssh cmd, bail out
+
+cd target\3.7.3\temp
+
+IF EXIST che-templates (
+    echo che-templates exist
+) ELSE (
+    mkdir che-templates
+)
+
+cd che-templates
+
+git clone https://github.com/codenvy-templates/web-jsp-java-basic.git
+git clone https://github.com/codenvy-templates/web-spring-java-simple.git
+git clone https://github.com/codenvy-templates/web-gwt-java-simple.git
+git clone https://github.com/codenvy-templates/web-grails-java-simple.git
+git clone https://github.com/codenvy-templates/web-jsf-java-simple.git
+git clone https://github.com/codenvy-templates/web-lift-java-simple.git
+git clone https://github.com/codenvy-templates/web-struts-java-simple.git
+git clone https://github.com/codenvy-templates/web-vaadin-java-simple.git
+git clone https://github.com/codenvy-templates/web-tapestry-java-simple.git
+git clone https://github.com/codenvy-templates/web-wicket-java-simple.git
+git clone https://github.com/codenvy-templates/web-groovy-java-simple.git
+git clone https://github.com/codenvy-templates/web-jruby-java-simple.git
+git clone https://github.com/codenvy-templates/web-jython-java-simple.git
+git clone https://github.com/codenvy-templates/web-shale-java-simple.git
+git clone https://github.com/codenvy-templates/web-velocity-java-simple.git
