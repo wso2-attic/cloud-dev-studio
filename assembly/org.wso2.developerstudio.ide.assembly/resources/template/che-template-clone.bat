29811fa91dc1f32dfdc4efd90ecf8e4c8915b998
rajeevanWSO2 <rajeevanv89@gmail.com>
2015-05-05 14:28:56 +0530

    Fixes for TOOLS-2782
    Upgrading Eclipse Che to 3.9.1

--- assembly/org.wso2.developerstudio.ide.assembly/resources/template/che-template-clone.bat
+++ assembly/org.wso2.developerstudio.ide.assembly/resources/template/che-template-clone.bat
@@ -6,7 +6,7 @@
 
 # if no output from the remote ssh cmd, bail out
 
-cd target\3.8.0\temp
+cd target\3.9.1\temp
 
 IF EXIST che-templates (
     echo che-templates exist
---
+++ extensions/artifacts/org.wso2.developerstudio.codenvy.ext.appfactory/src/main/java/org/wso2/developerstudio/codenvy/ext/appfactory/server/model/UserCredentials.java
@@ -0,0 +1,7 @@
+package org.wso2.developerstudio.codenvy.ext.appfactory.server.model;
+
+/**
+ * Created by rajeevanv on 4/27/15.
+ */
+public class UserCredentials {
+}
