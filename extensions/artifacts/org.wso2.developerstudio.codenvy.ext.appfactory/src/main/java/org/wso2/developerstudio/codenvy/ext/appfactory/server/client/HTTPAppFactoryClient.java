11d963b1c260461828a372dd433b832df68bb4d9
rajeevanWSO2 <rajeevanv89@gmail.com>
2015-05-19 14:14:49 +0530

    Removed unused imports

--- extensions/artifacts/org.wso2.developerstudio.codenvy.ext.appfactory/src/main/java/org/wso2/developerstudio/codenvy/ext/appfactory/server/client/HTTPAppFactoryClient.java
+++ extensions/artifacts/org.wso2.developerstudio.codenvy.ext.appfactory/src/main/java/org/wso2/developerstudio/codenvy/ext/appfactory/server/client/HTTPAppFactoryClient.java
@@ -15,7 +15,6 @@
 */
 package org.wso2.developerstudio.codenvy.ext.appfactory.server.client;
 
-import com.sun.tools.internal.ws.wsdl.document.http.HTTPConstants;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
